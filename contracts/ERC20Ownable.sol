e848ccb506b0899b68769ac7a6a7f1959f50caa4
Xopek1337 <83090357+Xopek1337@users.noreply.github.com>
2021-10-06 19:25:53 +0300

    Update ERC20Ownable.sol

--- contracts/ERC20Ownable.sol
+++ contracts/ERC20Ownable.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/ownership/Ownable.sol";
@@ -6,9 +7,9 @@ import "./ERC20Base.sol";
 contract ERC20own is IERC20, Ownable,ERC20Basic{
     function mint(address recipient, uint amount) public onlyOwner{
 
-        totalSupply+=amount;
-        balances[recipient]+=amount;
+        totalSupply += amount;
+        balances[recipient] += amount;
 
-        emit Transfer(address(0),recipient,amount);
+        emit Transfer(address(0), recipient, amount);
     }
 }
