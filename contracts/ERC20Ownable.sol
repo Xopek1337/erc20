b12c85e8faa7433b3ddf5864e64ed88d649c3b79
Xopek <nakit21@gmail.com>
2021-10-06 18:36:41 +0200

    lst

--- contracts/ERC20Ownable.sol
+++ contracts/ERC20Ownable.sol
@@ -1,14 +1,15 @@
-pragma solidity >=0.4.22 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "./ERC20Base.sol";
 
 contract ERC20own is IERC20, Ownable,ERC20Basic{
+
     function mint(address recipient, uint amount) public onlyOwner{
+        totalSupply += amount;
+        balances[recipient] += amount;
 
-        totalSupply+=amount;
-        balances[recipient]+=amount;
-
-        emit Transfer(address(0),recipient,amount);
+        emit Transfer(address(0), recipient, amount);
     }
 }
